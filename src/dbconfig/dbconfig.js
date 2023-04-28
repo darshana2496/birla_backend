a3bcbb671e8128d76e886fc695304c7385e5facd
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-04-28 12:48:06 +0530

    mubashir first commit

---
+++ src/dbconfig/dbconfig.js
@@ -0,0 +1,34 @@
+const { dbname, dbusername, dbpassword, dbhost } = require('../configs/variableconfig');
+
+const Sequelize = require('sequelize').Sequelize;
+
+const sequelize = new Sequelize( dbname , dbusername, dbpassword, { 
+    dialect: 'mssql',
+    host:dbhost,
+    logging:false,
+  }
+    )
+function connectDB(){
+        sequelize.authenticate().then(() => {
+          console.log('Connection has been established successfully.');
+        }).catch((error) => {
+          console.error('Unable to connect to the database: ', error);
+        });
+    }
+
+//     function checkTable (){
+//   sequelize.query("SELECT * FROM invoiceItems", { type: Sequelize.QueryTypes.SELECT })
+//   .then(results => {
+//     if (results.length > 0) {
+//       console.log('Table exists in the database.',results);
+//     } else {
+//       console.log('Table does not exist in the database.');
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Error checking for table existence:', error);
+//   });
+// }
+
+// checkTable ();
+module.exports = connectDB;
